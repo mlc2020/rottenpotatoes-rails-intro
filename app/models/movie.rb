30c8508bc164aaf5a5dbf474d63fb9767ae4d3a0
EC2 Default User <ec2-user@ip-172-31-45-187.us-west-2.compute.internal>
2021-02-26 02:48:19 +0000

    Testing Sort

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,18 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        self.find(:all, :select => "rating", :group => "rating").map(&:rating)
+        allRatings = []
+        Movie.all.each do |movie|
+          unless allRatings.include?(movie.rating)
+            allRatings << movie.rating
+          end
+        end
+        if allRatings.include?("NC-17")
+          allRatings.sort!
+          allRatings.delete("NC-17")
+          allRatings << "NC-17"
+          return allRatings
+        else
+          return allRatings.sort
+        end
     end
 end
